3d997364ae672fa4c3d683463b5354ebf138da40
Steve Short <short.steven.n@gmail.com>
2020-01-16 18:38:50 -0500

    initial commit

---
+++ SwashBuckleGenerationDemo/Controllers/Function1ApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SwashBuckleGenerationDemo.Controllers
+{
+    [ApiController]
+    [Route("[buddies]")]
+    public class Function1ApiController : ControllerBase
+    {
+        private readonly ILogger<Function1ApiController> logger;
+
+        public Function1ApiController(ILogger<Function1ApiController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [FunctionName(nameof(Get))]
+        [HttpGet]
+        public async Task Get([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "function1")] HttpRequest req)
+        {
+        }
+    }
+}
---
+++ SwashBuckleGenerationDemo/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Reflection;
+using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using SwashBuckleGenerationDemo;
+using SwashBuckleGenerationDemo.Controllers;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace SwashBuckleGenerationDemo
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+
+            var context = builder.Services.BuildServiceProvider()
+                .GetService<IOptions<ExecutionContextOptions>>().Value;
+
+            var currentDirectory = context.AppDirectory;
+
+            var configurationBuilder = new ConfigurationBuilder();
+            var configuration = configurationBuilder.SetBasePath(currentDirectory)
+                .AddJsonFile("host.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("local.settings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            ConfigureServices(builder.Services, configuration);
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+        }
+
+        private void ConfigureServices(IServiceCollection builder, IConfigurationRoot configuration)
+        {
+            // Register other project assemblies
+            builder.AddSingleton<Function1ApiController>();
+            builder.AddMvc();
+            builder.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo());
+            });
+
+            builder.AddSwaggerGen();
+        }
+    }
+}
